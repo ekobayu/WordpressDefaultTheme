1c0f66ce097f21af8f02569725904a1bfeba5b1c
Eko Bayu <eko.bayu.seo@gmail.com>
2025-02-16 11:51:46 +0800

    404 page, use getfield in repeater

--- 404.php
+++ 404.php
@@ -13,23 +13,15 @@
 
 get_header(); ?>
 
-<div class="wrap">
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-
-      <section class="error-404 not-found">
-        <header class="page-header">
-          <h1 class="page-title"><?php _e('Oops! That page can&rsquo;t be found.', 'twentyseventeen'); ?></h1>
-        </header><!-- .page-header -->
-        <div class="page-content">
-          <p><?php _e('It looks like nothing was found at this location. Maybe try a search?', 'twentyseventeen'); ?></p>
-
-          <?php get_search_form(); ?>
-
-        </div><!-- .page-content -->
-      </section><!-- .error-404 -->
-    </main><!-- #main -->
-  </div><!-- #primary -->
-</div><!-- .wrap -->
+<section class="home-page mt-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="page-title">Oops! That page can&rsquo;t be found.</h1>
+        <p>'It looks like nothing was found at this location.</p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer();
